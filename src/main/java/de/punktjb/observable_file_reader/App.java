65825922ba857c6c4bc69ff5f81c382f117e9a7c
Borozan, Jurica <Jurica.Borozan@opitz-consulting.com>
2018-03-12 23:41:21 +0100

    first working version

--- src/main/java/de/punktjb/observable_file_reader/App.java
+++ src/main/java/de/punktjb/observable_file_reader/App.java
@@ -1,14 +1,12 @@
 package de.punktjb.observable_file_reader;
 
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.function.Consumer;
 
 import rx.Observable;
 import rx.Observer;
 
 import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 
 /**
@@ -18,58 +16,89 @@ import java.io.FileNotFoundException;
  */
 public class App 
 {
-    public static void main( String[] args )
+	// buffer size
+	final int bufferSize = 1*1024;
+
+	// group size: email, name, surname
+	final int groupSize = 3;
+	
+	final String[] groupOfStrings = new String[groupSize];
+	int cnt = 0;
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main( String[] args )
+	{
+		// pick one
+		
+		// simple and nice formated data
+		//new App().simpleCase(args);
+		
+		// complex formated data
+		new App().complexCase(args);
+	}
+	
+	/**
+	 * If it is possible that lines of text are non-existing or do not end with \n or \r 
+	 * or it is not well formed - this solution shall be applied
+	 * uses Rxjava Observables and lambdas from java 8 
+	 * @param args
+	 */
+    public void complexCase( String[] args )
     {
     	
 		if(args.length > 0 && args[0].equals("-f"))
 		{
-			Observable<char[]> content = null;
+			Observable<char[]> fileContent = null;
+			
+			final StringBuilder collected = new StringBuilder();
 			
 			// input titles
 			try {
-				int size = 1024*1024;
 				
-				// create observable on file stream with buffer of 1024*1024 characters
-				content = Observable.create(new ObservableBufferedFileStream(new FileReader(args[1]), size));
+				// create observable on file stream with buffer of (x)*1024 characters
+				fileContent = Observable.create(new ObservableBufferedFileStream(new FileReader(args[1]), bufferSize));
 				
-				// and start the processing
-				content
-					.map( byteArray -> new String(byteArray) )	// convert byte array to string
-					.map( string -> string.split(";"))			// split it by ";"
-					.flatMapIterable( strings -> Arrays.<String>asList(strings))	// flatten map
-					.filter(string -> !string.isEmpty())		// if string not empty
-					.buffer(3)								// group them by 3: email, name, surname
-					.subscribe( new Observer<List<String>>() {	// do something with it
+				// and start the processing (of stream)
+				fileContent
+					.map( charArray -> new String(charArray) )		// convert char array to string (because observable do not work with primitive types)
+					.flatMap(string -> Observable.from(string.split("")))	// and then split to characters (but as strings)
+					.filter(string -> !string.isEmpty())	// remove empty ones
+					.filter(string -> !string.equals("\""))	// remove ["]
+					.filter(string -> !string.equals("\r"))	// remove [carriage return]
+					.subscribe( new Observer<String>() {	// do the processing
 
 						@Override
 						public void onCompleted() {
-
+							
 							System.out.println( "  ** Done!" );
 						}
 
 						@Override
 						public void onError(Throwable e) {
-
-							System.err.println( "  ** error: " +  e.getMessage() );
 							
+							System.err.println( "  ** error: " +  e.getMessage() );
 						}
 
 						@Override
-						public void onNext(List<String> t) {
-
-							// just print it for the time being
-							t.stream().forEach(new Consumer<String>() {
-
-								@Override
-								public void accept(String t) {
-									// TODO Auto-generated method stub
-									System.err.println( "  ** found: " +  t );
-								}
+						public void onNext(String t) {
+							
+							// collect characters
+							if( ";\n".contains(t) && collected.length() > 0 ) {
 								
-							});
-						}						
+								// store string and reset buffer
+								storeString(collected.toString());								
+								collected.setLength(0);								
+							}								
+							else {
+								collected.append(t);
+							}
+						}
+						
 					});
-				
+
 			} catch (FileNotFoundException e) {
 				
 				e.printStackTrace();
@@ -77,7 +106,70 @@ public class App
 		}
 		else
 		{
-		
+			System.err.println(" ** no arguments given");
 		}
+		
+		return;
     }
+    
+    protected void storeString(final String string) {
+
+    	groupOfStrings[cnt%groupSize] = string;
+		
+    	if(cnt++%groupSize == 2)
+    		sendMail(groupOfStrings);
+	}
+
+	/**
+     * If it is assumed that each line of file contains email address, name and sur name
+     * this would be simple solution, uses streams and lambdas from java 8
+     * 
+     * @param args
+     */
+    public void simpleCase( String[] args )
+    {
+
+		if(args.length > 0 && args[0].equals("-f"))
+		{
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(args[1]), bufferSize);
+				
+				bufferedReader.lines().forEach(new Consumer<String>() {
+
+					@Override
+					public void accept(String s) {
+
+						String[] splitted = s.replaceAll("[\"]", "").split("[;]");
+						
+						sendMail( splitted );
+					}
+				});
+				
+				bufferedReader.close();				
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			System.err.println(" ** no arguments given");
+		}
+		
+		return;
+    }
+
+    /**
+     * Email mock method
+     * @param strings
+     */
+	protected void sendMail(String[] strings) {
+
+		System.out.println(" ** sending email to " + strings[1] + " " + strings[2] + " <" + strings[0] + ">");
+		
+//		try {
+//			Thread.sleep(500);
+//		} catch (InterruptedException ignore) {}
+	}
+
 }
